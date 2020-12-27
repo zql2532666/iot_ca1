7baef45f32065694e85a6520c75a5c7e9c618967
zql2532666 <52409885+zql2532666@users.noreply.github.com>
2020-12-28 00:22:09 +0800

    Update dashboard1.js

--- app/static/js/pages/dashboards/dashboard1.js
+++ app/static/js/pages/dashboards/dashboard1.js
@@ -1,6 +1,6 @@
 function renderLDRChart(lightIntensity, labels){
     var ctx3 = document.getElementById("chart3").getContext('2d');
-    var myChart3 = new Chart(ctx3, {
+    lightChart = new Chart(ctx3, {
         type: 'line',
         data: {
             labels: labels,
@@ -12,16 +12,7 @@ function renderLDRChart(lightIntensity, labels){
                     borderWidth: 1,
                     fill: false
                 }
-            ],
-            options: {
-                scales: {
-                    xAxes: [
-                        {
-                            type: 'time'
-                        }
-                    ]
-                }
-            }
+            ]
         },
     });
 }
@@ -30,7 +21,7 @@ function renderHumidityChart(humidity, labels){
     var ctx2 = document.getElementById("chart2").getContext('2d');
 
     
-    var myChart2 = new Chart(ctx2, {
+    humidityChart = new Chart(ctx2, {
         type: 'line',
         data: {
             labels: labels,
@@ -42,16 +33,7 @@ function renderHumidityChart(humidity, labels){
                     borderWidth: 1,
                     fill: false
                 },
-            ],
-            options: {
-                scales: {
-                    xAxes: [
-                        {
-                            type: 'time'
-                        }
-                    ]
-                }
-            }
+            ]
         },
     });
 }
@@ -60,7 +42,7 @@ function renderTempChart(temp, labels) {
     var ctx = document.getElementById("chart").getContext('2d');
     
     
-    var myChart = new Chart(ctx, {
+    tempChart = new Chart(ctx, {
         type: 'line',
         data: {
             labels: labels,
@@ -72,99 +54,11 @@ function renderTempChart(temp, labels) {
                     borderWidth: 1,
                     fill: false
                 }
-            ],
-            options: {
-                scales: {
-                    xAxes: [
-                        {
-                            type: 'time'
-                        }
-                    ]
-                }
-            }
+            ]
         },
     });
 }
 
-var ctx = document.getElementById("chart").getContext('2d');
-var tempChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: labels,
-        datasets: [
-            {
-                label: 'temperature (Degree Celcius)',
-                data: temp,
-                borderColor: '#3c0e7b',
-                borderWidth: 1,
-                fill: false
-            }
-        ],
-        options: {
-            scales: {
-                xAxes: [
-                    {
-                        type: 'time'
-                    }
-                ]
-            }
-        }
-    },
-});
-
-
-var ctx2 = document.getElementById("chart2").getContext('2d');
-var humidityChart = new Chart(ctx2, {
-        type: 'line',
-        data: {
-            labels: labels,
-            datasets: [
-                {
-                    label: 'humidity (%)',
-                    data: humidity,
-                    borderColor: '#3b6431',
-                    borderWidth: 1,
-                    fill: false
-                },
-            ],
-            options: {
-                scales: {
-                    xAxes: [
-                        {
-                            type: 'time'
-                        }
-                    ]
-                }
-            }
-        },
-});
-
-
-var ctx3 = document.getElementById("chart3").getContext('2d');
-var lightChart = new Chart(ctx3, {
-        type: 'line',
-        data: {
-            labels: labels,
-            datasets: [
-                {
-                    label: 'Light Intensity',
-                    data: lightIntensity,
-                    borderColor: '#c56015',
-                    borderWidth: 1,
-                    fill: false
-                }
-            ],
-            options: {
-                scales: {
-                    xAxes: [
-                        {
-                            type: 'time'
-                        }
-                    ]
-                }
-            }
-        },
-});
 
 function getHistoricalTempAndHumidity() {
         var dht11Data = {}
@@ -209,16 +103,30 @@ function getHistoricalLightIntensity(){
         return LDRData
 };
 
-
-function updateGraphs(){
-    var ldrdata = getHistoricalLightIntensity();
+function renderCharts(){
     var dht11Data = getHistoricalTempAndHumidity();
+    var ldrdata = getHistoricalLightIntensity();
+
+
+    renderLDRChart(ldrdata.lightIntensity, ldrdata.datetime);
+    renderTempChart(dht11Data.temperatures, dht11Data.datetime);
+    renderHumidityChart(dht11Data.humidity, dht11Data.datetime);
 }
 
 
-function updateTempChart(dataset){
-    
+function updateCharts(){
+
+    $('#chart').remove();
+    $('#rchart2').remove();
+    $('#chart3').remove();
+
+    $('#temp-chart').append('<canvas class="w-100" height="400" id="chart"></canvas>');
+    $('#humidity-chart').append('<canvas class="w-100" height="400" id="chart2"></canvas>');
+    $('#light-chart').append('<canvas class="w-100" height="400" id="chart3"></canvas>');
+
+    renderCharts();
 }
+
 
 function getCurrentDHT11Data(){
     $.ajax({
@@ -242,16 +150,50 @@ function getCurrentLDRData(){
     });
 };
 
+function getLEDStatus(){
+    $.ajax({
+        url: "/api/led-status",
+        success: function(results){
+                    console.log(results);
+                    if (results['led_status'] == true){
+                        $('#led_status').html('LED is on');
+                    }
+                    else{
+                        $('#led_status').html('LED is off');
+                    }
+                },
+        type: 'GET'
+    });
+};
 
 
 $(document).ready(function() {
-    getLEDStatus();
-    updateGraphs();
+    //getLEDStatus();
+    renderCharts();
     setInterval(getCurrentLDRData, 1000 * 5);
     setInterval(getCurrentDHT11Data, 1000 * 5);
 });
 
 
+function callLedOnAPI(){
+    $.ajax({
+        url: "/api/led-on",
+        success: function(results){
+                    getLEDStatus();
+                },
+        type: 'GET'
+    });
+};
+
+function callLedOffAPI(){
+    $.ajax({
+        url: "/api/led-off",
+        success: function(results){
+                    getLEDStatus();
+                },
+        type: 'GET'
+    });
+};
 
 
-
+// setInterval(updateGraphs, 1000 * 5);

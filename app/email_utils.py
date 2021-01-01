c38ce1d348c3207b2c064b72077387dbc83d9557
Zhang Rongtao <849217645@qq.com>
2021-01-01 23:42:46 +0800

    cmt

--- app/email_utils.py
+++ app/email_utils.py
@@ -1,8 +1,12 @@
 import smtplib, ssl
 from time import sleep
 import database_utils
-import app
 
+# variables for MYSQL Database connection 
+HOST = "localhost"
+USER = "root"
+PASSWORD = "root"
+DATABASE = "iot_ca1"
 
 SMTP_SERVER = "smtp.gmail.com"
 PORT = 587
@@ -10,7 +14,7 @@ PORT = 587
 SENDER_EMAIL = "iot.ca1.emailbot@gmail.com"
 PASSWORD = "1234qwer$#@!"
 
-mysql_connection, mysql_cursor = database_utils.get_mysql_connection(app.HOST, app.USER, app.PASSWORD, app.DATABASE)
+mysql_connection, mysql_cursor = database_utils.get_mysql_connection(HOST, USER, PASSWORD, DATABASE)
 receiver_email = database_utils.get_user_info(mysql_cursor)['email']
 mysql_connection.close()
 print(receiver_email)

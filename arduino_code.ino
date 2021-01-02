665c84a2aa7cd711c19536e81c545fbd6e8f4d61
kygahd <849217645@qq.com>
2021-01-02 17:49:34 +0800

    cmt

---
+++ arduino_code.ino
@@ -0,0 +1,14 @@
+// on Arduino
+int sensorPin = A0;
+
+int sensorValue = 0;
+
+void setup(){
+  Serial.begin(9600);
+}
+
+void loop(){
+  sensorValue = analogRead(sensorPin);
+  Serial.println(sensorValue);
+  delay(5000); // sleep for 5 seconds
+}
